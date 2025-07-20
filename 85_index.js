62de4a68193a9c66e3cb3cdb1783c66cfa70ec2c
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-07-20 07:05:40 +0300

    Create 85_index.js

---
+++ 85_index.js
@@ -0,0 +1,8 @@
+
+// let div = document.querySelector('div');
+// let content = div.childNodes;
+// console.log((content[content.length-1].textContent.trim()));
+
+document.addEventListener("click" , (e) => {
+    console.log(`This is ${e.target.tagName.toLowerCase()}`);
+})
