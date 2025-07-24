6ecd68d672a8af7c5253c87ce7c9a2f595f864c9
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-07-24 23:17:23 +0300

    Create 95_index.js

---
+++ 95_index.js
@@ -0,0 +1,39 @@
+let chosen = 3;
+
+let myFriends = [
+    { title: "Osama", age: 39, available: true, skills: ["HTML", "CSS"] },
+    { title: "Ahmed", age: 25, available: false, skills: ["Python", "Django"] },
+    { title: "Sayed", age: 33, available: true, skills: ["PHP", "Laravel"] },
+];
+
+// Write Your Code Here
+
+let {
+    title,
+    age,
+    available,
+    skills: [, l],
+} = myFriends[chosen - 1];
+
+console.log(`${title} ${age}${available? "" : " Not"} Available ${l}`)
+// console.log(title, age, available, l);
+// If chosen === 1
+
+// "Osama"
+// 39
+// "Available"
+// "CSS"
+
+// // If chosen === 2
+
+// "Ahmed"
+// 25
+// "Not Available"
+// "Django"
+
+// // If chosen === 3
+
+// "Sayed"
+// 33
+// "Available"
+// "Laravel"
