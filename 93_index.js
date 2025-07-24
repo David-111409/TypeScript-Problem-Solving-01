07edeb7f45423c6282ee0011e8f8130667c99069
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-07-24 23:22:16 +0300

    Update 93_index.js

--- 93_index.js
+++ 93_index.js
@@ -21,7 +21,7 @@
 // let arr1 = ["Ahmed", "Sameh", "Sayed"];
 // let arr2 = ["Mohamed", "Gamal", "Amir"];
 // let arr3 = ["Haytham", "Shady", "Mahmoud"];
-
+// let [, a, b, c] = [...arr3, ...arr1];
 // Play With Arrays To Prepare For Destructuring
 // const [c , , , , a, b] = [...arr1, ...arr3];
 // console.log(`My Best Friends: ${a}, ${b}, ${c}`);
